75c54e1b2ebc35ac00c2bc7de5fb1b448e921a65
Rayllanderson <rayllanderson@gmail.com>
2020-07-23 11:09:02 -0300

    menu produto concluido, mas nao testado

--- src/model/service/ProductService.java
+++ src/model/service/ProductService.java
@@ -37,7 +37,60 @@ public class ProductService {
 	for (int i = 0 ; i< list.size(); i++) {
 	    FormatarTabela.printInvoice(list.get(i), maxLenName + 2, 5, (i+1));
 	}
-	
     }
-
+    
+    public Product getProdutoByNumer(int num) {
+	List<Product> list = dao.findAll();
+	if (list.isEmpty()) {
+	    throw new ProductoException("Ops, parece que você não tem nenhum produto na lista.");
+	}
+	if (num > list.size()) {
+	    throw new ProductoException("Parece não existe nenhum produto com número " + num + ". Verifique a tabela e tente novamente");
+	}
+	return list.get(num);
+    }
+    
+    public boolean atualizar(Product p) {
+	try {
+	    dao.atualizar(p);
+	    return true;
+	}catch (DbException e) {
+	    return false;
+	}
+    }
+    
+    public boolean deletar(Product p) {
+	try {
+	    dao.deletById(p.getId());
+	    return true;
+	}catch (DbException e) {
+	    return false;
+	}
+    }
+    
+    public void editarNome(Product p, String nome) {
+	p.setNome(nome);
+	dao.atualizar(p);
+    }
+    
+    public void editarPrecoEstipulado(Product p, double value) {
+	p.setPrecoEstipulado(value);
+	dao.atualizar(p);
+    }
+    
+    public void editarPrecoReal(Product p, double value) {
+	p.setPrecoReal(value);
+	dao.atualizar(p);
+    }
+    
+    public void marcarComoConcluido(Product p, double value) {
+	if (p.isCompraro()) {
+	    throw new ProductoException("Produto já marcado como comprado");
+	}
+	p.setPrecoReal(value);
+	p.setCompraro(true);
+	dao.atualizar(p);
+    }
+    
+    //talvez editar categoria futuramente...
 }
--- src/model/view/TelaPrincipal.java
+++ src/model/view/TelaPrincipal.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 import model.dao.DaoFactory;
 import model.dao.TelaLoginDao;
 import model.entities.Categoria;
+import model.entities.Product;
 import model.entities.User;
 import model.exception.BackButtonException;
 import model.exception.CategoriaException;
 import model.exception.MyLoginException;
+import model.exception.ProductoException;
 import model.service.CategoriaService;
+import model.service.ProductService;
 
 public class TelaPrincipal {
 
@@ -87,9 +90,45 @@ public class TelaPrincipal {
 		scan.next();
 	    } catch (CategoriaException e) {
 		System.out.println(e.getMessage());
-	    }catch (BackButtonException e) {
+	    } catch (BackButtonException e) {
 	    }
 	} while (true);
+    }
+
+    public boolean telaProduto(Categoria cat) {
+	ProductService service = new ProductService(cat);
+	while (true) {
+	    service.listarPordutos();
+	    this.menuProdutos();
+	    try {
+		int opcao = scan.nextInt();
+		switch (opcao) {
+		case 0:
+		    System.exit(0);
+		case 1:
+		    this.adicionarProduto(service, cat);
+		case 2:
+		    while (this.menuEditarProduto(service));
+		    break;
+		case 3:
+		    Product p = selecionarProduto(service, "Excluir");
+		    service.deletar(p);
+		    break;
+		case 4:
+		    Product p1 = selecionarProduto(service, "Marcar como comprado");
+		    this.marcarComoConcluido(service, p1);
+		    break;
+		default:
+		    throw new InputMismatchException();
+		}
+	    } catch (InputMismatchException e) {
+		System.out.println("Opção inválida! Tente novamente.");
+		scan.next();
+	    } catch (ProductoException e) {
+		System.out.println(e.getMessage());
+	    }
+	}
+
     }
 
     private void menuCategorias() {
@@ -98,6 +137,23 @@ public class TelaPrincipal {
 	System.out.println("[ 3 ] - Editar Lista");
 	System.out.println("[ 4 ] - Excluir Lista");
 	System.out.println("[ 0 ] - Sair");
+    }
+
+    private void menuProdutos() {
+	System.out.println("[ 1 ] - Adicionar Novo Produto");
+	System.out.println("[ 2 ] - Editar Produto");
+	System.out.println("[ 3 ] - Excluir Produto");
+	System.out.println("[ 4 ] - Marcar como comprado");
+	System.out.println("[ 0 ] - Sair");
+    }
+
+    private void menuEditarProduto() {
+	System.out.println("[ 1 ] - Editar tudo");
+	System.out.println("[ 2 ] - Editar nome");
+	System.out.println("[ 3 ] - Editar Preço Estipulado");
+	System.out.println("[ 4 ] - Editar Preço Real");
+	System.out.println("[ 5 ] - Marcar como comprado");
+	System.out.println("[ 0 ] - Voltar");
     }
 
     private String formatarNome(String nome) {
@@ -158,7 +214,105 @@ public class TelaPrincipal {
 	int numCategoria = scan.nextInt();
 	botaoVoltar(numCategoria);
 	Categoria cat1 = service.getCategoriaByNumber(numCategoria);
-	if (service.deletarCategoria(cat1)) System.out.println("Lista deletada com sucesso!");;
+	if (service.deletarCategoria(cat1))
+	    System.out.println("Lista deletada com sucesso!");
+	;
+    }
+
+    private void adicionarProduto(ProductService service, Categoria cat) {
+	System.out.print("Nome: ");
+	String nome = scan.nextLine();
+	System.out.print("Qual o preço que você acha que vai pagar? R$");
+	double valorEstipulado = scan.nextDouble();
+	System.out.println("OBS: se você não comprou o produto ainda, deixe 0");
+	System.out.print("Qual o preço que você realmente pagou? R$");
+	double valorReal = scan.nextDouble();
+	Product p = new Product(null, nome, valorEstipulado, valorReal, false, cat.getUser(), cat);
+	if (!(valorReal == 0)) {
+	    service.marcarComoConcluido(p, valorReal);
+	}
+	service.inserir(p);
+    }
+
+    private boolean menuEditarProduto(ProductService service) {
+	while (true) {
+	    try {
+		Product p = selecionarProduto(service, "Editar");
+		menuEditarProduto();
+		int opcaoEditarProduto = scan.nextInt();
+		switch (opcaoEditarProduto) {
+		case 0:
+		    return true;
+		case 1:
+		    editarProduto(service, p);
+		    break;
+		case 2:
+		    System.out.print("Novo nome: ");
+		    String name = scan.nextLine();
+		    service.editarNome(p, name);
+		    break;
+		case 3:
+		    System.out.print("Novo valor estipulado: R$");
+		    double valorEstipulado = scan.nextDouble();
+		    service.editarPrecoEstipulado(p, valorEstipulado);
+		    break;
+		case 4:
+		    editarValorReal(service, p);
+		    break;
+		case 5:
+		    marcarComoConcluido(service, p);
+		    break;
+		default:
+		    throw new InputMismatchException("Opção inválida");
+		}
+	    } catch (InputMismatchException e) {
+		System.out.println(e.getMessage());
+		scan.next();
+	    }
+	}
+    }
+
+    private void editarProduto(ProductService service, Product p) {
+	System.out.print("Nome: ");
+	String nome = scan.nextLine();
+	System.out.print("Qual o preço que você acha que vai pagar? R$");
+	double valorEstipulado = scan.nextDouble();
+	System.out.println("OBS: se você não comprou o produto ainda, deixe 0");
+	System.out.print("Qual o preço que você realmente pagou? R$");
+	double valorReal = scan.nextDouble();
+	if (!(valorReal == 0)) {
+	    service.marcarComoConcluido(p, valorReal);
+	}
+	p.setNome(nome);
+	p.setPrecoEstipulado(valorEstipulado);
+	service.atualizar(p);
+    }
+
+    private void marcarComoConcluido(ProductService service, Product p) {
+	if (p.getPrecoReal() != 0) {
+	    System.out.println("O preço real atual é de R$" + p.getPrecoReal() + ". Deseja alterar esse valor?");
+	    System.out.println("[ 1 ] - sim");
+	    System.out.println("[ 2 ] - nop");
+	    if (scan.nextInt() == 1) {
+		editarValorReal(service, p);
+	    }
+	}
+	double value = p.getPrecoReal();
+	service.marcarComoConcluido(p, value);
+    }
+
+    private void editarValorReal(ProductService service, Product p) {
+	System.out.print("Valor real: R$");
+	double valorReal = scan.nextDouble();
+	service.editarPrecoReal(p, valorReal);
+    }
+    
+    private Product selecionarProduto(ProductService service, String acao) {
+	service.listarPordutos();
+	System.out.print("Esolha qual produto deseja " + acao + ": ");
+	int produtoEscolhido = scan.nextInt();
+	Product p = service.getProdutoByNumer(produtoEscolhido);
+	return p;
     }
 
 }
