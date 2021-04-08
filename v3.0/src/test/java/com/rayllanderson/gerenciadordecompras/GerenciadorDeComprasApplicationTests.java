899bce05c58f41fbaad141caaf6ec8b22f90ba6d
Rayllanderson <rayllanderson@gmail.com>
2021-04-08 05:58:12 -0300

    Criando projeto Spring Boot

---
+++ v3.0/src/test/java/com/rayllanderson/gerenciadordecompras/GerenciadorDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayllanderson.gerenciadordecompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeComprasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
