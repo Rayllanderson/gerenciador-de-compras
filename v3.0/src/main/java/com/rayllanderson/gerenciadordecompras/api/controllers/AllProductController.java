616007a95849be43840c534dd27439b0d3d09bf1
Rayllanderson <rayllanderson@gmail.com>
2021-04-12 13:17:38 -0300

    AllProductController

---
+++ v3.0/src/main/java/com/rayllanderson/gerenciadordecompras/api/controllers/AllProductController.java
@@ -0,0 +1,95 @@
+package com.rayllanderson.gerenciadordecompras.api.controllers;
+
+import com.rayllanderson.gerenciadordecompras.core.dtos.product.AllProductPostRequestBody;
+import com.rayllanderson.gerenciadordecompras.core.dtos.product.ProductPostResponseBody;
+import com.rayllanderson.gerenciadordecompras.core.dtos.product.ProductPutRequestBody;
+import com.rayllanderson.gerenciadordecompras.core.model.Product;
+import com.rayllanderson.gerenciadordecompras.core.requests.SelectItemsRequestBody;
+import com.rayllanderson.gerenciadordecompras.core.requests.StatisticResponseBody;
+import com.rayllanderson.gerenciadordecompras.core.requests.products.TransferAllProductRequestBody;
+import com.rayllanderson.gerenciadordecompras.core.services.AllProductService;
+import com.rayllanderson.gerenciadordecompras.core.services.StatisticService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/all-products")
+public class AllProductController {
+
+    private final StatisticService statisticService;
+    private final AllProductService allProductService;
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> findAll(Pageable pageable) {
+        Long userId = 1L;
+        return ResponseEntity.ok(allProductService.findAll(userId, pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Long userId = 1L;
+        return ResponseEntity.ok(allProductService.findById(id, userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductPostResponseBody> save(@RequestBody AllProductPostRequestBody productPostRequestBody){
+        Long userId = 1L;
+        return ResponseEntity.status(HttpStatus.CREATED).body(allProductService.save(productPostRequestBody, userId));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody ProductPutRequestBody productPutRequestBody){
+        Long userId = 1L;
+        productPutRequestBody.setId(id);
+        allProductService.update(productPutRequestBody, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        Long userId = 1L;
+        allProductService.deleteById(id, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteVarious(@RequestBody List<SelectItemsRequestBody> ids){
+        Long userId = 1L;
+        allProductService.deleteVariousById(ids, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<Product>> findByName(@RequestParam String name, Pageable pageable){
+        Long userId = 1L;
+        return ResponseEntity.ok(allProductService.findByName(name, userId, pageable));
+    }
+
+    @PostMapping("/copy")
+    public ResponseEntity<Void> copyProductsToAnotherCategory(@RequestBody TransferAllProductRequestBody data){
+        Long userId = 1L;
+        allProductService.copyProductsToAnotherCategory(data, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/move")
+    public ResponseEntity<Void> moveProductsToAnotherCategory(@RequestBody TransferAllProductRequestBody data){
+        Long userId = 1L;
+        allProductService.moveProductsToAnotherCategory(data, userId);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/statistics")
+    public ResponseEntity<StatisticResponseBody> getStatistics() {
+        Long userId = 1L;
+        return ResponseEntity.ok(statisticService.getStatistics(userId));
+    }
+}
