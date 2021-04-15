fb186cb50c4b57c71761110e9cbcb220ad989d10
Rayllanderson <rayllanderson@gmail.com>
2021-04-15 10:38:27 -0300

    Organizando files

--- v3.0/src/main/java/com/rayllanderson/gerenciadordecompras/domain/dtos/user/JwtLoginInput.java
+++
@@ -1,9 +0,0 @@
-package com.rayllanderson.gerenciadordecompras.domain.dtos.user;
-
-import lombok.Data;
-
-@Data
-public class JwtLoginInput {
-    private String username;
-    private String password;
-}
