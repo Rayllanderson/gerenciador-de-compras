8a13f52f7114091100c0dc6a2874203fe9ae2ba0
Ray <63964369+Rayllanderson@users.noreply.github.com>
2021-04-24 04:21:16 -0300

    Update WebSecurityConfig.java

--- v3.0/api/src/main/java/com/rayllanderson/gerenciadordecompras/api/security/WebSecurityConfig.java
+++ v3.0/api/src/main/java/com/rayllanderson/gerenciadordecompras/api/security/WebSecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 
 @EnableWebSecurity
