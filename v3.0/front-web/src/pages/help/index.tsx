e1ff957a53654e59749da4ef5e4014c9b2a1100f
Rayllanderson <rayllanderson@gmail.com>
2021-05-12 02:54:17 -0300

    Help page -> produtos

--- v3.0/front-web/src/pages/help/index.tsx
+++ v3.0/front-web/src/pages/help/index.tsx
@@ -19,15 +19,14 @@ export function HelpPage() {
                 <SummaryItem href={'#duplicateList'}>Duplicando Listas</SummaryItem>
 
                 <SummaryHeader className={'pt-3'}> Produtos </SummaryHeader>
-                <SummaryItem>Como eu adiciono um produto?</SummaryItem>
-                <SummaryItem>O que significa Valor do produto?</SummaryItem>
-                <SummaryItem>O que significa valor gasto?</SummaryItem>
-                <SummaryItem>Como editar um produto?</SummaryItem>
-                <SummaryItem>Como editar um produto?</SummaryItem>
-                <SummaryItem>Como apaga um produto?</SummaryItem>
-                <SummaryItem>Como apaga vários produtos de uma vez?</SummaryItem>
-                <SummaryItem>Como copiar um produto pra outra lista?</SummaryItem>
-                <SummaryItem>Como mover um produto pra outra lista?</SummaryItem>
+                <SummaryItem href={'#addProduct'}>Como eu adiciono um produto?</SummaryItem>
+                <SummaryItem href={'#stipulatedPrice'}>O que significa Valor do produto?</SummaryItem>
+                <SummaryItem href={'#spentPrice'}>O que significa valor gasto?</SummaryItem>
+                <SummaryItem href={'#editProduct'}>Como editar um produto?</SummaryItem>
+                <SummaryItem href={'#removeProduct'}>Como apaga um produto?</SummaryItem>
+                <SummaryItem href={'#removeMultiplesProducts'}>Como apaga vários produtos de uma vez?</SummaryItem>
+                <SummaryItem href={'#copyProducts'}>Como copiar produtos pra outra lista?</SummaryItem>
+                <SummaryItem href={'#moveProducts'}>Como mover produtos pra outra lista?</SummaryItem>
 
                 <SummaryHeader className={'pt-3'}>Card de Produtos</SummaryHeader>
                 <SummaryItem>O que significa Valor Atual?</SummaryItem>
@@ -78,6 +77,66 @@ export function HelpPage() {
                 e a nova lista terá o nome da original seguido de 'cópia' </p>
                 <Image className={'img-fluid rounded '} id={'options'}
                        src={"https://github.com/Rayllanderson/assets/blob/master/gerenciador-compras/ScreenShot_20210512014627.png?raw=true"}/>
+
+
+                <HelpContentHeader id={'addProduct'}>Como eu adiciono um produto?</HelpContentHeader>
+                <p> Para adicionar uma nova lista, basta clicar no símbolo de + (Mais) como o da imagem abaixo. </p>
+                <Image className={'img-fluid rounded '}
+                       src={"https://github.com/Rayllanderson/assets/blob/master/gerenciador-compras/ScreenShot_20210512014627.png?raw=true"}/>
+                <p> Após clicar, irá surgir uma tela e apenas o nome é obrigatório.</p>
+
+
+                <HelpContentHeader id={'stipulatedPrice'}>O que significa Valor do produto?</HelpContentHeader>
+                <p>
+                    Significa o valor que você acha que vai pagar no produto, um valor estipulado, um valor que
+                    você tem em mente para comprar o produto.
+                </p>
+                <p> Exemplificando:
+                    Digamos que você esteja querendo comprar uma Geladeira. Você faz uma rápida pesquisa na internet
+                    e olha os preços médios, após isso você deve ter um preço em mente, certo? É esse preço que você
+                    colocará aqui, um preço estipulado.
+                </p>
+                <Image className={'img-fluid rounded '}
+                       src={"https://github.com/Rayllanderson/assets/blob/master/gerenciador-compras/ScreenShot_20210512014627.png?raw=true"}/>
+
+                <HelpContentHeader id={'spentPrice'}>O que significa valor gasto?</HelpContentHeader>
+                <p>
+                    Significa o valor que você realmente pagou no produto após ter comprado.
+                    Lembra do valor estipulado? Que era o preço que você achava que iria pagar?
+                    Então, aqui você coloca o valor que realmente pagou. Pode ser ele maior ou menor que o preço esperado,
+                    afinal de contas, nem sempre os preços saem mais baratos que a gente espera.
+                </p>
+                <Image className={'img-fluid rounded '}
+                       src={"https://github.com/Rayllanderson/assets/blob/master/gerenciador-compras/ScreenShot_20210512014627.png?raw=true"}/>
+
+                <HelpContentHeader id={'editProduct'}>Como editar um produto?</HelpContentHeader>
+                <Image className={'img-fluid rounded '}
+                       src={"https://github.com/Rayllanderson/assets/blob/master/gerenciador-compras/ScreenShot_20210512014627.png?raw=true"}/>
+                <p>Após clicar nos 3 pontos (como o da imagem) basta selecionar <strong>Editar</strong>, e então editar o produto que deseja</p>
+
+                <HelpContentHeader id={'removeProduct'}>Como apaga um produto?</HelpContentHeader>
+                <Image className={'img-fluid rounded '}
+                       src={"https://github.com/Rayllanderson/assets/blob/master/gerenciador-compras/ScreenShot_20210512014627.png?raw=true"}/>
+                <p>Após clicar nos 3 pontos (como o da imagem) basta selecionar <strong>Remover</strong>, e então editar o produto que deseja</p>
+
+                <HelpContentHeader id={'removeMultiplesProducts'}>Como apaga vários produtos de uma vez?</HelpContentHeader>
+                <p>Clicando nos <a href={'#options'}>3 pontinhos</a> Selecione a opção <strong>Selecionar</strong>
+                    agora é só selecionar os produtos que deseja remover </p>
+
+                <HelpContentHeader id={'copyProducts'}>Como copiar produtos pra outra lista?</HelpContentHeader>
+                <p>Clicando nos <a href={'#options'}>3 pontinhos</a> Selecione a opção <strong>Selecionar. </strong>
+                Agora selecione os produtos que deseja copiar para outra lista. Depois de selecionados,
+                    basta clicar em <strong>Copiar</strong></p>
+                <Image className={'img-fluid rounded '}
+                       src={"https://github.com/Rayllanderson/assets/blob/master/gerenciador-compras/ScreenShot_20210512014627.png?raw=true"}/>
+
+                <HelpContentHeader id={'moveProducts'}>Como mover produtos pra outra lista?</HelpContentHeader>
+                <p>Clicando nos <a href={'#options'}>3 pontinhos</a> Selecione a opção <strong>Selecionar. </strong>
+                    Agora selecione os produtos que deseja copiar para outra lista. Depois de selecionados,
+                    basta clicar em <strong>Mover</strong></p>
+                <Image className={'img-fluid rounded '}
+                       src={"https://github.com/Rayllanderson/assets/blob/master/gerenciador-compras/ScreenShot_20210512014627.png?raw=true"}/>
+
             </HelpContent>
 
         </HelpCardContainer>
